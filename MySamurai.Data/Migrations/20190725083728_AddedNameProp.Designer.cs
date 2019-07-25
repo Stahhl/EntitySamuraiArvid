7dc3ccd6ba24f643116007910a4199df1e60ea1a
stahhl <arvostahl@gmail.com>
2019-07-25 10:39:07 +0200

    cleaned solution, added samurai name prop

--- MySamurai.Data/Migrations/20190725083728_AddedNameProp.Designer.cs
+++ MySamurai.Data/Migrations/20190725083728_AddedNameProp.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MySamurai.Data.Models;
 
 namespace MySamurai.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20190725083728_AddedNameProp")]
+    partial class AddedNameProp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MySamurai.Domain/ClassLibrary/Samurai.cs
+++ MySamurai.Domain/ClassLibrary/Samurai.cs
@@ -7,5 +7,6 @@ namespace MySamurai.Domain.ClassLibrary
     public class Samurai
     {
         public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
